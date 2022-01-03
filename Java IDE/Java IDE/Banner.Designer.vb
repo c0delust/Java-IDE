1547fb8d8a39c14b8ee7dbfebcb1f75d00a2ec96
Mohammadsaad Mulla <83002941+codelust@users.noreply.github.com>
2022-01-04 01:13:33 +0530

    Add files via upload

---
+++ Java IDE/Java IDE/Banner.Designer.vb
@@ -0,0 +1,46 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Banner
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Banner
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Java_IDE.My.Resources.Resources.Banner
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(400, 300)
+        Me.ControlBox = False
+        Me.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Banner"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Banner"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ Java IDE/Java IDE/Form2.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.nSave = New System.Windows.Forms.Button()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(12, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(206, 18)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Do you want to save Changes"
+        Me.Label1.UseWaitCursor = True
+        '
+        'Cancel
+        '
+        Me.Cancel.Location = New System.Drawing.Point(263, 67)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.Cancel.TabIndex = 6
+        Me.Cancel.Text = "Cancel"
+        Me.Cancel.UseVisualStyleBackColor = True
+        Me.Cancel.UseWaitCursor = True
+        '
+        'nSave
+        '
+        Me.nSave.Location = New System.Drawing.Point(182, 67)
+        Me.nSave.Name = "nSave"
+        Me.nSave.Size = New System.Drawing.Size(75, 23)
+        Me.nSave.TabIndex = 5
+        Me.nSave.Text = "Don't Save"
+        Me.nSave.UseVisualStyleBackColor = True
+        Me.nSave.UseWaitCursor = True
+        '
+        'Save
+        '
+        Me.Save.Location = New System.Drawing.Point(101, 67)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(75, 23)
+        Me.Save.TabIndex = 4
+        Me.Save.Text = "Save"
+        Me.Save.UseVisualStyleBackColor = True
+        Me.Save.UseWaitCursor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(350, 102)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.nSave)
+        Me.Controls.Add(Me.Save)
+        Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form2"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Java IDE"
+        Me.TopMost = True
+        Me.UseWaitCursor = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Cancel As Button
+    Friend WithEvents nSave As Button
+    Friend WithEvents Save As Button
+End Class
